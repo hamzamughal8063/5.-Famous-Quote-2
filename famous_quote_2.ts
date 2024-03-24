0f22efb57a14b94c9f795d34aba3901ef992a228
Hamza Mughal <163909829+hamzamughal8063@users.noreply.github.com>
2024-03-25 00:16:12 +0500

    Famous Quote 2

---
+++ famous_quote_2.ts
@@ -0,0 +1,5 @@
+let famous_person :string = 'Albert Einstein';
+
+let messege :string = '"A person who made a mistake never tried anything new."';
+
+console.log(`${famous_person} once said, ${messege}`);
